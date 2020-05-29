e7dcc4d6708f48a73c54bf0d5116a736d7554736
Cao My <caomy97nc@gmail.com>
2020-05-29 22:02:52 +0700

    4

--- app/src/main/java/com/example/myticket/TimKiemActivity.java
+++ app/src/main/java/com/example/myticket/TimKiemActivity.java
@@ -1,9 +1,11 @@
 package com.example.myticket;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -14,15 +16,26 @@ import java.util.List;
 
 public class TimKiemActivity extends AppCompatActivity {
     private Spinner spnCategory, spnCategory2;
+    Button btnOK, btnThoat;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate (savedInstanceState);
         setContentView (R.layout.layout_timkiem);
         spnCategory = (Spinner) findViewById (R.id.spnCategory);
         spnCategory2 = (Spinner)findViewById (R.id.spnCategory2) ;
+        btnOK = (Button) findViewById (R.id.btnOK);
+        btnThoat = (Button) findViewById (R.id.btnThoat);
 
+        btnOK.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (TimKiemActivity.this , ChonGheActivity.class);
+                startActivity (intent);
+            }
+        });
         List<String> list = new ArrayList<> ();
         list.add("Điểm 1");
         list.add("Điểm 2");
@@ -71,5 +84,7 @@ public class TimKiemActivity extends AppCompatActivity {
 
             }
         });
+
     }
+
 }
